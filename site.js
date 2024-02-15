d6a004acb8c1718a70702e23c90ef0a18f93ddf5
bigslime892001 <donkorfrancis1000@icloud.com>
2024-02-15 12:36:55 +0000

    added an object

--- site.js
+++ site.js
@@ -1,5 +1,18 @@
-let studentName = "francis Donkor";
+/* let studentName = "francis Donkor";
 
 const studentAge = 21;
 
-console.log(`${studentName} is ${studentAge} years old`)
+console.log(`${studentName} is ${studentAge} years old`) */
+
+
+const studentDetails ={
+    studentName : 'Francis Donkor',
+    studentId : '11332199',
+    studentAge : 22,
+    courses : {
+        programming : 'A',
+        webDevelopment : 'B+'
+    },
+
+}
+console.log(studentDetails.courses)
